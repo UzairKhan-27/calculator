f801c30f7073848131ca1368bbe26af948b224a1
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-20 22:59:42 +0500

    Display selected numbers on screen

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@ let subtract=(a,b)=>a-b;
 let multiply=(a,b)=>a*b;
 let divide=(a,b)=>a/b;
 let operate=(firstNumber,secondNumber,operator)=> {
-
     let result=undefined;
     switch(operator)
     {
@@ -25,3 +24,73 @@ let operate=(firstNumber,secondNumber,operator)=> {
     }
     return result;
 };
+const digitKeys=document.querySelectorAll(".digits button");
+
+// let array=Array.from(allKeys);
+// console.log(allKeys.length);
+// console.log(array[0]);
+// const num9=document.querySelector("#num9");
+// num9.addEventListener("click",event=>{
+//     console.log(allKeys.item(17).id);
+// });      converted to array
+
+let firstNumber,secondNumber,operator=undefined;
+let display=0
+let screen=document.querySelector(".screen");
+screen.textContent="";
+
+digitKeys.forEach((key)=>{
+    console.log(key.id);
+    key.addEventListener("click",(event)=>{
+        
+        // getNumber(key);
+        switch(key.id)
+        {
+            
+            case "num0":
+                console.log(key.id);
+                screen.textContent+=0;
+                break;
+            case "num1":
+                console.log(key.id);
+                screen.textContent+=1;
+                break;
+            case "num2":
+                console.log(key.id);
+                screen.textContent+=2;
+                break;
+            case "num3":
+                console.log(key.id);
+                screen.textContent+=3;
+                break;
+            case "num4":
+                console.log(key.id);
+                screen.textContent+=4;
+                break;
+            case "num5":
+                console.log(key.id);
+                screen.textContent+=5;
+                break;
+            case "num6":
+                console.log(key.id);
+                screen.textContent+=6;
+                break;
+            case "num7":
+                console.log(key.id);
+                screen.textContent+=7;
+                break;
+            case "num8":
+                console.log(key.id);
+                screen.textContent+=8;
+                break;
+            case "num9":
+                console.log(key.id);
+                screen.textContent+=9;
+                break;
+            default:
+                console.log("error");
+                break;
+        }
+        
+    });
+});
